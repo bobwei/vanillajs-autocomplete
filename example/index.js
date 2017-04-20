0b40735dd6cd826e51e2589db37f5dbd4800bc63
bobwei <bob61016@gmail.com>
2017-04-21 00:46:48 +0800

    add multiple inputs to example page

--- example/index.js
+++ example/index.js
@@ -5,9 +5,12 @@ import items from './data/items';
 import './index.scss';
 
 document.addEventListener('DOMContentLoaded', () => {
-  const $input0 = document.getElementById('input0');
-  AutoComplete({
-    el: $input0,
-    data: items,
-  });
+  Array
+    .from(document.querySelectorAll('input'))
+    .forEach(el => (
+      AutoComplete({
+        el,
+        data: items,
+      })
+    ));
 });
