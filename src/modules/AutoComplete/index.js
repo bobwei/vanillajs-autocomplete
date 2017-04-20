23f037376fa981bdfe7923133d02fb1489184f75
bobwei <bob61016@gmail.com>
2017-04-20 21:04:52 +0800

    be able to render Option component, use data as initialState to create store

--- src/modules/AutoComplete/index.js
+++ src/modules/AutoComplete/index.js
@@ -1,18 +1,23 @@
 import createElement from 'modules/dom/createElement';
 import insertBefore from 'modules/dom/insertBefore';
+import Option from 'modules/AutoComplete/components/Option';
+import createStore from 'modules/stores/createStore';
 
 const createAutoComplete = ({
   el,
   data,
 }) => {
+  const store = createStore({ data });
+  const { getState } = store;
   const refs = {};
+
   refs.$input = el;
+  refs.$input.oninput = console.log;
+
   refs.$optionList = createElement(
     'div',
     {},
-    data.map(({ label }) => (
-      createElement('div', {}, label)
-    )),
+    getState().data.map(props => createElement(Option, props)),
   );
 
   insertBefore(refs.$optionList, refs.$input);
