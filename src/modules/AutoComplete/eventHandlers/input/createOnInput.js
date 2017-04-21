794a60fbaf38432c8f6ab733f2383c9a18836b83
bobwei <bob61016@gmail.com>
2017-04-21 15:53:14 +0800

    reset focusIndex on oninput

--- src/modules/AutoComplete/eventHandlers/input/createOnInput.js
+++ src/modules/AutoComplete/eventHandlers/input/createOnInput.js
@@ -5,7 +5,12 @@ const createOnInput = ({
   filterOption = ({ q }) => ({ label }) => label.indexOf(q) > -1,
 }) => (e) => {
   const q = valueSelector(e);
-  setState({ data: data.filter(filterOption({ q })) });
+  /* reset focusIndex to force update */
+  setState({ focusIndex: -1 });
+  setState({
+    data: data.filter(filterOption({ q })),
+    focusIndex: 0,
+  });
 };
 
 export default createOnInput;
